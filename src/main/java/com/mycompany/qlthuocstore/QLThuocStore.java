5c26b2beab0b2a717f006739f911c0c1a2766558
phuc5807 <phuctran5807@gmail.com>
2024-11-03 23:37:50 +0700

    Day code thuoc tay

---
+++ src/main/java/com/mycompany/qlthuocstore/QLThuocStore.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.qlthuocstore;
+
+/**
+ *
+ * @author Phuc
+ */
+public class QLThuocStore {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
